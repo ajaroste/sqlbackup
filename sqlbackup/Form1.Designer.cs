cbdab257c64592dd741aa67eae94b9a546140b54
Administrator <Administrator@WIN-IMKBR9JS0K7>
2024-09-29 22:19:45 +0300

    Proje dosyası ekle.

---
+++ sqlbackup/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace sqlbackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TBServer = new DevExpress.XtraEditors.TextEdit();
+            this.CBsql = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.TBUsername = new DevExpress.XtraEditors.TextEdit();
+            this.TBPassworld = new DevExpress.XtraEditors.TextEdit();
+            this.CBDatabase = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.BtnConnection = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnnBackup = new DevExpress.XtraEditors.SimpleButton();
+            this.btnEditKlasorYolu = new DevExpress.XtraEditors.ButtonEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBServer.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CBsql.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBUsername.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBPassworld.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CBDatabase.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEditKlasorYolu.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TBServer
+            // 
+            this.TBServer.Location = new System.Drawing.Point(83, 84);
+            this.TBServer.Name = "TBServer";
+            this.TBServer.Size = new System.Drawing.Size(195, 20);
+            this.TBServer.TabIndex = 0;
+            // 
+            // CBsql
+            // 
+            this.CBsql.Location = new System.Drawing.Point(83, 39);
+            this.CBsql.Name = "CBsql";
+            this.CBsql.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.CBsql.Properties.Items.AddRange(new object[] {
+            "Mssql",
+            "Mysql",
+            "Posgresql"});
+            this.CBsql.Size = new System.Drawing.Size(195, 20);
+            this.CBsql.TabIndex = 1;
+            // 
+            // TBUsername
+            // 
+            this.TBUsername.Location = new System.Drawing.Point(83, 131);
+            this.TBUsername.Name = "TBUsername";
+            this.TBUsername.Size = new System.Drawing.Size(195, 20);
+            this.TBUsername.TabIndex = 2;
+            // 
+            // TBPassworld
+            // 
+            this.TBPassworld.Location = new System.Drawing.Point(83, 172);
+            this.TBPassworld.Name = "TBPassworld";
+            this.TBPassworld.Size = new System.Drawing.Size(195, 20);
+            this.TBPassworld.TabIndex = 3;
+            // 
+            // CBDatabase
+            // 
+            this.CBDatabase.Location = new System.Drawing.Point(31, 316);
+            this.CBDatabase.Name = "CBDatabase";
+            this.CBDatabase.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.CBDatabase.Size = new System.Drawing.Size(299, 20);
+            this.CBDatabase.TabIndex = 4;
+            // 
+            // BtnConnection
+            // 
+            this.BtnConnection.Location = new System.Drawing.Point(89, 224);
+            this.BtnConnection.Name = "BtnConnection";
+            this.BtnConnection.Size = new System.Drawing.Size(188, 37);
+            this.BtnConnection.TabIndex = 6;
+            this.BtnConnection.Text = "Bağlantıyı Test Et";
+            this.BtnConnection.Click += new System.EventHandler(this.BtnConnection_Click);
+            // 
+            // BtnnBackup
+            // 
+            this.BtnnBackup.Location = new System.Drawing.Point(89, 413);
+            this.BtnnBackup.Name = "BtnnBackup";
+            this.BtnnBackup.Size = new System.Drawing.Size(188, 37);
+            this.BtnnBackup.TabIndex = 7;
+            this.BtnnBackup.Text = "Yedekle";
+            this.BtnnBackup.Click += new System.EventHandler(this.BtnnBackup_Click);
+            // 
+            // btnEditKlasorYolu
+            // 
+            this.btnEditKlasorYolu.Location = new System.Drawing.Point(31, 354);
+            this.btnEditKlasorYolu.Name = "btnEditKlasorYolu";
+            this.btnEditKlasorYolu.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.btnEditKlasorYolu.Properties.NullValuePromptShowForEmptyValue = true;
+            this.btnEditKlasorYolu.Size = new System.Drawing.Size(299, 20);
+            this.btnEditKlasorYolu.TabIndex = 8;
+            this.btnEditKlasorYolu.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.btnEditKlasorYolu_ButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 519);
+            this.Controls.Add(this.btnEditKlasorYolu);
+            this.Controls.Add(this.BtnnBackup);
+            this.Controls.Add(this.BtnConnection);
+            this.Controls.Add(this.CBDatabase);
+            this.Controls.Add(this.TBPassworld);
+            this.Controls.Add(this.TBUsername);
+            this.Controls.Add(this.CBsql);
+            this.Controls.Add(this.TBServer);
+            this.MaximumSize = new System.Drawing.Size(374, 558);
+            this.MinimumSize = new System.Drawing.Size(374, 558);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TBServer.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CBsql.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBUsername.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBPassworld.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CBDatabase.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEditKlasorYolu.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit TBServer;
+        private DevExpress.XtraEditors.ComboBoxEdit CBsql;
+        private DevExpress.XtraEditors.TextEdit TBUsername;
+        private DevExpress.XtraEditors.TextEdit TBPassworld;
+        private DevExpress.XtraEditors.ComboBoxEdit CBDatabase;
+        private DevExpress.XtraEditors.SimpleButton BtnConnection;
+        private DevExpress.XtraEditors.SimpleButton BtnnBackup;
+        private DevExpress.XtraEditors.ButtonEdit btnEditKlasorYolu;
+    }
+}
+
---
+++ sqlbackup/Form1.cs
@@ -0,0 +1,67 @@
+﻿
+using DevExpress.XtraEditors;
+using sqlbackup.Enums;
+using sqlbackup.Manager;
+using System;
+using System.Windows.Forms;
+
+
+namespace sqlbackup
+{
+    public partial class Form1 : Form
+    { 
+        public SqlEnum SQL;
+        public bool connection;
+        public Form1()
+        {
+            SQL = (SqlEnum)CBsql.SelectedIndex;
+            InitializeComponent();
+        }
+      
+        private void BtnConnection_Click(object sender, EventArgs e)
+        {
+            CBDatabase.Properties.Items.Clear();
+    
+            var x = SqlConnector.SqlConnection(SQL, TBServer.Text, TBUsername.Text, TBPassworld.Text);
+
+            if (x != null)
+            {
+                foreach (var item in x)
+                {
+                    CBDatabase.Properties.Items.Add(item);
+                }
+                connection = true;
+
+            }
+            else
+            {
+                XtraMessageBox.Show("Bağlantı Başarısız bro :)");
+                connection = false;
+            }
+        }
+
+        private void BtnnBackup_Click(object sender, EventArgs e)
+        {
+           
+
+
+
+
+
+
+
+
+        }
+
+        private void btnEditKlasorYolu_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFolder = folderBrowserDialog.SelectedPath;
+                btnEditKlasorYolu.Text = selectedFolder;
+            }
+        }
+    }
+}
